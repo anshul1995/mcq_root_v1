51adcac74114886ad64cedaa9b9f0a462b7fe245
Anshul Aggarwal <aanshul@umich.edu>
2020-05-07 18:09:51 -0400

    updated instructor question model

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Question(models.Model):
+class Base_Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
@@ -13,10 +13,27 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    class Meta:
+        abstract = True
+
+class Question(Base_Question):
+    TYPE1 = 'T1'
+    TYPE2 = 'T2'
+    QUESTION_TYPE_CHOICES = [
+        (TYPE1, 'For all students'),
+        (TYPE2, 'For students not creating a question'),
+    ]
+    questiion_type = models.CharField(
+        max_length=2,
+        choices=QUESTION_TYPE_CHOICES,
+        default=TYPE1,
+    )
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    is_correct = models.BooleanField(default=False)
     def __str__(self):
         return self.choice_text
