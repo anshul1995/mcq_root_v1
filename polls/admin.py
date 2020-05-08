e430acbbcadc98bc3856564ca4a50a1559303f0d
Anshul Aggarwal <aanshul@umich.edu>
2020-05-07 20:29:52 -0400

    added student model

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question, Choice
+from .models import Question, Choice, Student_Question, Student_Choice, Student
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -16,6 +16,24 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
 
+
+class StudentChoiceInline(admin.TabularInline):
+    model = Student_Choice
+
+
+class StudentQuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text', 'explanation_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [StudentChoiceInline]
+
 admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(Choice)
+
+admin.site.register(Student_Question, StudentQuestionAdmin)
+
+admin.site.register(Student_Choice)
+
+admin.site.register(Student)
--- polls/models.py
+++ polls/models.py
@@ -16,6 +16,21 @@ class Base_Question(models.Model):
     class Meta:
         abstract = True
 
+
+class Base_Choice(models.Model):
+    choice_text = models.CharField(max_length=2000)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
+    class Meta:
+        abstract = True
+
+
+######## INSTRUCTOR QUESTIONS ########
+
+
 class Question(Base_Question):
     TYPE1 = 'T1'
     TYPE2 = 'T2'
@@ -29,20 +44,33 @@ class Question(Base_Question):
         default=TYPE1,
     )
 
-class Student_Question(Base_Question):
-    explanation_text = models.CharField(max_length=2000)
-
-
-class Base_Choice(models.Model):
-    choice_text = models.CharField(max_length=200)
-    is_correct = models.BooleanField(default=False)
-    def __str__(self):
-        return self.choice_text
-
-    class Meta:
-        abstract = True
-
 
 class Choice(Base_Choice):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     votes = models.IntegerField(default=0)
+
+
+######## STUDENT QUESTIONS ########
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    def get_group(self):
+        return 1 + hash(self.name)%3
+
+
+class Student_Question(Base_Question):
+    explanation_text = models.CharField(max_length=2000)
+    by_student = models.OneToOneField(
+        Student,
+        on_delete=models.CASCADE,
+        related_name='creator_of',
+        null=True,
+    )
+
+
+class Student_Choice(Base_Choice):
+    question = models.ForeignKey(Student_Question, on_delete=models.CASCADE)
