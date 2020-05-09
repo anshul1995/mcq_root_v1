99d04a604795175634a8a2ad7a86634e0b51c351
Anshul Aggarwal <aanshul@umich.edu>
2020-05-08 22:30:05 -0400

    changes in admin to make some fields readonly

--- polls/admin.py
+++ polls/admin.py
@@ -12,34 +12,38 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text', 'question_type']}),
-        # ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    readonly_fields = ('pub_date',)
     inlines = [ChoiceInline]
 
 
 class StudentChoiceInline(admin.TabularInline):
     model = Student_Choice
+    readonly_fields = ('question',)
     extra = 0
 
 
 class StudentQuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text', 'explanation_text', 'by_student']}),
-        # ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    readonly_fields = ('pub_date',)
     inlines = [StudentChoiceInline]
 
 
 class StudentResponseInline(admin.TabularInline):
     model = Student_Response
+    readonly_fields = ('student_id','question_id','choice_id',)
+    can_delete = False
     extra = 0
-
 
 class StudentAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Student information', {'fields': ['name', 'attempted', 'group', 'id']}),
     )
-    readonly_fields = ('id',)
+    readonly_fields = ('id','name','attempted',)
     inlines = [StudentResponseInline]
 
 admin.site.register(Question, QuestionAdmin)
