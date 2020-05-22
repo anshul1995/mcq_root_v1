e05df67e7616a95efde68a21f8ff2173bef9a2d7
Anshul Aggarwal <aanshul@umich.edu>
2020-05-22 02:15:21 -0400

    minor change in js

--- polls/static/polls/group_3_form.js
+++ polls/static/polls/group_3_form.js
@@ -70,12 +70,5 @@ $(function () {
         // these HTTP methods do not require CSRF protection
         return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
     }
-    // $.ajaxSetup({
-    //     beforeSend: function (xhr, settings) {
-    //         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-    //             xhr.setRequestHeader("X-CSRFToken", csrftoken);
-    //         }
-    //     }
-    // });
 
 });
